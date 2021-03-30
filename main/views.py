3da54c339339e89bf2ee21d7d3fdf48f5aa9e684
TanimSk <67966003+TanimSk@users.noreply.github.com>
2021-03-31 00:03:40 +0600

    Add files via upload

---
+++ main/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from . import conimg, eval
+
+global img1, img2
+
+
+def home(req):
+    return render(req, 'home\\index.html')
+
+
+def omrs(req):
+    if req.method == 'POST' and req.FILES['upload_img']:
+        global img1
+        img1 = conimg.resize_img(req.FILES['upload_img'])
+
+    return render(req, 'omrs\\index.html')
+
+
+def show(req):
+    if req.method == 'POST' and req.FILES['upload_img']:
+        global img1, img2
+        img2 = conimg.resize_img(req.FILES['upload_img'])
+        img = eval.eval(img1, img2)
+
+        return render(req, 'show\\index.html', {'img': "data:image/jpg;base64," + img})
+    else:
+        return HttpResponse('<center><h1>not found !</h1></center>')
