8395a9ee62ef00f032ea112a59a2d8f4b33b420a
TanimSk <sktanim5800@gmail.com>
2021-04-04 14:25:48 +0600

    Update settings.py
    removed root dir

--- omr_scanner/settings.py
+++ omr_scanner/settings.py
@@ -121,10 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'root')
-
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
+        BASE_DIR / "static",
 ]
 
 TEMPLATE_DIRS = (
