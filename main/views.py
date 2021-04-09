0da5b59db1b083890e1012d3fb66c83b40fa8a36
TanimSk <sktanim5800@gmail.com>
2021-04-09 15:38:10 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,32 +1,29 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-from . import conimg, eval
+from . import conimg, eval, models
 
 global img1, img2
 
 
 def home(req):
+    if req.method == 'POST' and req.FILES['upload_img']:
+        global img1
+        img1 = conimg.resize_img(req.FILES['upload_img'])
+
     return render(req, '../templates/home/index.html')
 
 
 def omrs(req):
     if req.method == 'POST' and req.FILES['upload_img']:
-        global img1
-        img1 = conimg.resize_img(req.FILES['upload_img'])
-    
+        global img1, img2
+        img2 = conimg.resize_img(req.FILES['upload_img'])
+        img2 = eval.eval(img1, img2)
+
     return render(req, 'omrs/index.html')
 
 
 def show(req):
-    if req.method == 'POST' and req.FILES['upload_img']:
-        global img1, img2
-        img2 = conimg.resize_img(req.FILES['upload_img'])
-        img = eval.eval(img1, img2)
-
-        return render(req, 'show/index.html', {'img': "data:image/jpg;base64," + img})
-    else:
-        return HttpResponse('<center><h1>not found !</h1></center>')
+    return render(req, 'show/index.html', {'img': "data:image/jpg;base64," + img2})
 
 
 def about(req):
@@ -34,4 +31,11 @@ def about(req):
 
 
 def support(req):
+    if req.method == 'POST':
+        name = req.POST.get('name')
+        email = req.POST.get('email')
+        amount = req.POST.get('amount')
+        entry = models.Donor(name=name, email=email, amount=amount)
+        entry.save()
+
     return render(req, 'make-me-happy/index.html')
